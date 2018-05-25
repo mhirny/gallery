4f69d5681617133860bc4d75de6521b0ba92cd65
Marcin Hirny <mphirny@gmail.com>
2018-05-25 16:15:24 +0200

    First commit

---
+++ _MySQL/gallery.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.16  Distrib 10.1.28-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: galery
+-- ------------------------------------------------------
+-- Server version	10.1.28-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pictures`
+--
+
+DROP TABLE IF EXISTS `pictures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pictures` (
+  `picID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `location` varchar(100) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `description` text,
+  `price` decimal(13,2) DEFAULT NULL,
+  `tag` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`picID`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pictures`
+--
+
+LOCK TABLES `pictures` WRITE;
+/*!40000 ALTER TABLE `pictures` DISABLE KEYS */;
+INSERT INTO `pictures` VALUES (1,'img/trabant.jpg','Trabant','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',90.20,'vehicle'),(2,'img/art/gears.jpg','Gears','Lorem ipsum dolor.',19.20,'art'),(3,'img/art/smiles.jpg','Smiles','Lorem ipsum dolor sit amet, consectetur adipisicing elit, unt ut labore et.',1000.00,'art'),(4,'img/art/eiffel.jpg','Eiffel tower','Lorem ipsum sit, consectetur adipisicing elit, unt ut labore et.',0.03,'art'),(5,'img/art/deathsbride.jpg','Death\'s bride','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi, fugi',33.33,'art'),(6,'img/art/housepaint.jpg','Houses','Lorem ipsum dolor sit amet, consectetur adipisicing elit.',3.37,'art'),(7,'img/art/robot.jpg','MrRobot','Lorem ipsum dolor sit elit.',99.99,'art'),(8,'img/art/saltnpepper.jpg','Salt N\' Pepper','Lorem ipsum dolor sit elit. Sunt, laudantium, quibusdam?',19.23,'art'),(9,'img/art/statue.jpg','Statue','Lorem ipsum elit. Sunt, quibusdam?',100000.00,'art'),(10,'img/vehicles/vehicle1.jpg','Galaxie','Lorem ipsum elit. Sunt, quibusdam?',199000.00,'vehicle'),(11,'img/vehicles/vehicle2.jpg','Bentley','Lorem ipsum elit. Sunt, quibusdam?',1500000.00,'vehicle'),(12,'img/vehicles/vehicle3.jpg','Mercedes','Lorem ipsum elit. Facilis consequatur, odio hic minima. Sunt, quibusdam?',1000000.00,'vehicle'),(13,'img/vehicles/vehicle4.jpg','Red-Bus','Lorem ipsum elit. Facilis consequatur, odio hic minima.',500000.00,'vehicle'),(14,'img/vehicles/vehicle5.jpg','Lowrider','Lorem ipsum elit. Facilis consequatur, odio hic minima.',2500000.00,'vehicle'),(15,'img/vehicles/vehicle6.jpg','BMW bike','Lorem ipsum elit. Facilis consequatur, odio hic minima.',752000.00,'vehicle'),(16,'img/vehicles/vehicle7.jpg','Mustang?','Lorem ipsum elit. Facilis consequatur, odio hic minima.',1752000.00,'vehicle'),(17,'img/vehicles/vehicle8.jpg','Lamborghini','Lorem ipsum elit. Facilis consequatur, odio hic minima.',3752000.00,'vehicle'),(18,'img/vehicles/vehicle9.jpg','Chevrolet','Lorem ipsum elit. Facilis consequatur, odio hic minima.',752000.00,'vehicle'),(19,'img/vehicles/vehicle10.jpg','Kawasaki','Lorem ipsum elit. Facilis consequatur, odio hic minima.',820000.00,'vehicle'),(20,'img/vehicles/vehicle11.jpg','Alfa Romeo','Lorem ipsum elit. Facilis consequatur, odio hic minima.',2820000.00,'vehicle');
+/*!40000 ALTER TABLE `pictures` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-05-25 15:38:10
---
+++ art.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Gallery | Art</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+<!-- NAV -->
+
+  <nav class="navbar navbar-default">
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.html"><img src="./img/palete_small_t.png" alt="Art!"></a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="cars.php">Cars</a></li>
+          <li class="active"><a href="art.php">Art <span class="sr-only">(current)</span></a></li>
+        </ul>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">About</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container -->
+  </nav>
+
+<!-- MAIN -->
+
+  <main id="art-page">
+    <div id="bg"></div>
+    <div class="container">
+      <div class="row">
+
+
+          <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $db = "gallery";
+
+            $conn = mysqli_connect($servername, $username, $password, $db) or die("Connection failed: " . mysqli_connect_error());
+
+            $sql = 'SELECT * FROM pictures WHERE tag = "art";';
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+              // output data of each row
+              while($row = mysqli_fetch_assoc($result)) {
+echo '
+              <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                <div class="panel panel-default center-block">
+                  <div class="panel-heading">
+                    <h2>'.$row["name"].'</h2>
+                    <img src="'.$row["location"].'" style="width: 100%;" class="thumbnail">
+                  </div>
+                  <div class="panel-body">
+                    '.$row["description"].'
+                  </div>
+                  <div class="panel-footer">
+                    <p class="text-right">Price: '.$row["price"].'$</p>
+                  </div>
+                </div>
+              </div>
+';
+              }
+            }
+            mysqli_close($conn);
+          ?>
+
+        
+      </div>
+    </div>
+  </main>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
