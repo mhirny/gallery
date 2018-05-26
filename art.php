0af7c91de9199027450641f6b90491a28072f1aa
Marcin Hirny <mphirny@gmail.com>
2018-05-26 18:43:51 +0200

    added sql to nav-right: user name display + basket

--- art.php
+++ art.php
@@ -20,19 +20,47 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.html"><img src="./img/palete_small_t.png" alt="Art!"></a>
+        <a class="navbar-brand" href="index.php"><img src="./img/palete_small_t.png" alt="Art!"></a>
       </div>
 
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="navbar-collapse">
         <ul class="nav navbar-nav">
-          <li><a href="index.html">Home</a></li>
+          <li><a href="index.php">Home</a></li>
           <li><a href="cars.php">Cars</a></li>
           <li class="active"><a href="art.php">Art <span class="sr-only">(current)</span></a></li>
         </ul>
 
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">About</a></li>
+          <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $db = "gallery";
+
+            $conn = mysqli_connect($servername, $username, $password, $db) or die("Connection failed: " . mysqli_connect_error());
+
+            $sql = 'SELECT users.fname, users.lname, COUNT(basket.personID) AS in_basket FROM users, basket WHERE users.personID = "1" AND users.personID = basket.personID;';
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+              // output data of each row
+              while($row = mysqli_fetch_assoc($result)) {
+                // <span class="glyphicon glyphicon-shopping-cart" style="font-size:20px;"></span>
+                // <span class="badge glyphicon glyphicon-shopping-cart" style="font-size:30px; line-height:30px;">  '.$row["in_basket"].'</span>
+echo '
+                <li><a href="#">
+                  <span>'.$row["fname"].' '.$row["lname"].'</span>
+                </a></li>
+                <li><a href="#" style="padding-top:7px; padding-bottom:8px; ">
+
+                  <span class="glyphicon glyphicon-shopping-cart" style="font-size:30px;"><span class="badge" style="margin-left:-23px; margin-top: -35px; background: rgb(121,207,169);">  '.$row["in_basket"].'</span></span>
+                </a></li>
+';
+              }
+            }
+            mysqli_close($conn);
+          ?>
         </ul>
       </div><!-- /.navbar-collapse -->
     </div><!-- /.container -->
