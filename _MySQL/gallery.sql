22fd0c1887bd90c263b35e7790025a64fcffd5ad
Marcin Hirny <mphirny@gmail.com>
2018-05-28 14:26:56 +0200

    refreshed MySQL DB

--- _MySQL/gallery.sql
+++ _MySQL/gallery.sql
@@ -58,7 +58,6 @@ CREATE TABLE `pictures` (
   `name` varchar(50) DEFAULT NULL,
   `description` text,
   `price` decimal(13,2) DEFAULT NULL,
-  `tag` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`picID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -69,8 +68,33 @@ CREATE TABLE `pictures` (
 
 LOCK TABLES `pictures` WRITE;
 /*!40000 ALTER TABLE `pictures` DISABLE KEYS */;
-INSERT INTO `pictures` VALUES (1,'img/vehicles/trabant.jpg','Trabant','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',90.20,'vehicle'),(2,'img/art/gears.jpg','Gears','Lorem ipsum dolor.',19.20,'art'),(3,'img/art/smiles.jpg','Smiles','Lorem ipsum dolor sit amet, consectetur adipisicing elit, unt ut labore et.',1000.00,'art'),(4,'img/art/eiffel.jpg','Eiffel tower','Lorem ipsum sit, consectetur adipisicing elit, unt ut labore et.',0.03,'art'),(5,'img/art/deathsbride.jpg','Death\'s bride','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi, fugi',33.33,'art'),(6,'img/art/housepaint.jpg','Houses','Lorem ipsum dolor sit amet, consectetur adipisicing elit.',3.37,'art'),(7,'img/art/robot.jpg','MrRobot','Lorem ipsum dolor sit elit.',99.99,'art'),(8,'img/art/saltnpepper.jpg','Salt N\' Pepper','Lorem ipsum dolor sit elit. Sunt, laudantium, quibusdam?',19.23,'art'),(9,'img/art/statue.jpg','Statue','Lorem ipsum elit. Sunt, quibusdam?',100000.00,'art'),(10,'img/vehicles/vehicle1.jpg','Galaxie','Lorem ipsum elit. Sunt, quibusdam?',199000.00,'vehicle'),(11,'img/vehicles/vehicle2.jpg','Bentley','Lorem ipsum elit. Sunt, quibusdam?',1500000.00,'vehicle'),(12,'img/vehicles/vehicle3.jpg','Mercedes','Lorem ipsum elit. Facilis consequatur, odio hic minima. Sunt, quibusdam?',1000000.00,'vehicle'),(13,'img/vehicles/vehicle4.jpg','Red-Bus','Lorem ipsum elit. Facilis consequatur, odio hic minima.',500000.00,'vehicle'),(14,'img/vehicles/vehicle5.jpg','Lowrider','Lorem ipsum elit. Facilis consequatur, odio hic minima.',2500000.00,'vehicle'),(15,'img/vehicles/vehicle6.jpg','BMW bike','Lorem ipsum elit. Facilis consequatur, odio hic minima.',752000.00,'vehicle'),(16,'img/vehicles/vehicle7.jpg','Mustang?','Lorem ipsum elit. Facilis consequatur, odio hic minima.',1752000.00,'vehicle'),(17,'img/vehicles/vehicle8.jpg','Lamborghini','Lorem ipsum elit. Facilis consequatur, odio hic minima.',3752000.00,'vehicle'),(18,'img/vehicles/vehicle9.jpg','Chevrolet','Lorem ipsum elit. Facilis consequatur, odio hic minima.',752000.00,'vehicle'),(19,'img/vehicles/vehicle10.jpg','Kawasaki','Lorem ipsum elit. Facilis consequatur, odio hic minima.',820000.00,'vehicle'),(20,'img/vehicles/vehicle11.jpg','Alfa Romeo','Lorem ipsum elit. Facilis consequatur, odio hic minima.',2820000.00,'vehicle');
+INSERT INTO `pictures` VALUES (1,'img/vehicles/trabant.jpg','Trabant','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',90.20),(2,'img/art/gears.jpg','Gears','Lorem ipsum dolor.',19.20),(3,'img/art/smiles.jpg','Smiles','Lorem ipsum dolor sit amet, consectetur adipisicing elit, unt ut labore et.',1000.00),(4,'img/art/eiffel.jpg','Eiffel tower','Lorem ipsum sit, consectetur adipisicing elit, unt ut labore et.',0.03),(5,'img/art/deathsbride.jpg','Death\'s bride','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi, fugi',33.33),(6,'img/art/housepaint.jpg','Houses','Lorem ipsum dolor sit amet, consectetur adipisicing elit.',3.37),(7,'img/art/robot.jpg','MrRobot','Lorem ipsum dolor sit elit.',99.99),(8,'img/art/saltnpepper.jpg','Salt N\' Pepper','Lorem ipsum dolor sit elit. Sunt, laudantium, quibusdam?',19.23),(9,'img/art/statue.jpg','Statue','Lorem ipsum elit. Sunt, quibusdam?',100000.00),(10,'img/vehicles/vehicle1.jpg','Galaxie','Lorem ipsum elit. Sunt, quibusdam?',199000.00),(11,'img/vehicles/vehicle2.jpg','Bentley','Lorem ipsum elit. Sunt, quibusdam?',1500000.00),(12,'img/vehicles/vehicle3.jpg','Mercedes','Lorem ipsum elit. Facilis consequatur, odio hic minima. Sunt, quibusdam?',1000000.00),(13,'img/vehicles/vehicle4.jpg','Red-Bus','Lorem ipsum elit. Facilis consequatur, odio hic minima.',500000.00),(14,'img/vehicles/vehicle5.jpg','Lowrider','Lorem ipsum elit. Facilis consequatur, odio hic minima.',2500000.00),(15,'img/vehicles/vehicle6.jpg','BMW bike','Lorem ipsum elit. Facilis consequatur, odio hic minima.',752000.00),(16,'img/vehicles/vehicle7.jpg','Mustang?','Lorem ipsum elit. Facilis consequatur, odio hic minima.',1752000.00),(17,'img/vehicles/vehicle8.jpg','Lamborghini','Lorem ipsum elit. Facilis consequatur, odio hic minima.',3752000.00),(18,'img/vehicles/vehicle9.jpg','Chevrolet','Lorem ipsum elit. Facilis consequatur, odio hic minima.',752000.00),(19,'img/vehicles/vehicle10.jpg','Kawasaki','Lorem ipsum elit. Facilis consequatur, odio hic minima.',820000.00),(20,'img/vehicles/vehicle11.jpg','Alfa Romeo','Lorem ipsum elit. Facilis consequatur, odio hic minima.',2820000.00);
 /*!40000 ALTER TABLE `pictures` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `picID` bigint(20) DEFAULT NULL,
+  `tag` varchar(15) DEFAULT NULL,
+  KEY `picID` (`picID`),
+  CONSTRAINT `tags_ibfk_1` FOREIGN KEY (`picID`) REFERENCES `pictures` (`picID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tags`
+--
+
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+INSERT INTO `tags` VALUES (1,'vehicle'),(1,'art'),(2,'art'),(3,'art'),(4,'art'),(5,'art'),(6,'art'),(7,'art'),(8,'art'),(9,'art'),(10,'vehicle'),(11,'vehicle'),(12,'vehicle'),(13,'vehicle'),(14,'vehicle'),(15,'vehicle'),(16,'vehicle'),(17,'vehicle'),(18,'vehicle'),(19,'vehicle'),(20,'vehicle');
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -108,4 +132,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-26 18:45:40
+-- Dump completed on 2018-05-28 14:25:53
